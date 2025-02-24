50d4b36a13a8ab0c585b4ae4f6d1d29a6e790b89
vidhi1234 <vidhideshmukh95@gmail.com>
2025-02-24 14:37:10 +0530

    vidhi commit

--- Flipkart/clothing/urls.py
+++ Flipkart/clothing/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from clothing import views
 urlpatterns=[
-    path('',views.clothing,name="clothing")
+    path('',views.clothing,name="clothing"),
+    path('edit/<int:product_id>/',views.edit_product,name='edit_product'),
+    path('delete/<int:product_id>/',views.delete_product,name='delete_product')
 ]
--- Flipkart/clothing/views.py
+++ Flipkart/clothing/views.py
@@ -1,14 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404,redirect
 from clothing.models import Product
 from .forms import ProductForm
+
 # Create your views here.
 def clothing(request):
     form=ProductForm()
     pro_img=Product.objects.all()
     
     if request.method == "POST":
-        form=ProductForm(request.POST, request.FILES)
+        form=ProductForm(request.POST, request.FILES,instance=product)
         if form.is_valid(): 
             form.save()
             form=ProductForm() # reset form after saving
     return render(request,'clothing/fashion.html',{'pro_img':pro_img,'form':form})
+
+def edit_product(request,product_id):
+    product=get_object_or_404(Product,id=product_id)
+
+    if request.method == "POST":
+        form=ProductForm(request.POST, request.FILES,instance=Product)
+        if form.is_valid(): 
+            form.save()
+            return redirect('clothing')
+    else:
+        form=ProductForm(instance=product)
+    
+    return render(request, 'clothing/edit_product.html', {'form':form})
+
+def delete_product(request,product_id):
+    product=get_object_or_404(Product,id=product_id)
+
+    if request.method == "POST":
+        product.delete()
+        return redirect('clothing')
+        
+    return render(request,'clothing/confirm_delete.html',{'product':product})
